eba48897f23dbc4641f5de361cd2cb6aad69dcde
yohei <yohei@yoheinoair.lan>
2021-01-22 19:36:16 +0900

    Updates

---
+++ precache-manifest.024a1d28e08948f95225becf3e3cabac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07328ccd5e75398246e950f7fba5d8ba",
+    "url": "/skipper113/testhub/index.html"
+  },
+  {
+    "revision": "e74be43bfdd09cc81f23",
+    "url": "/skipper113/testhub/static/css/main.910aa548.chunk.css"
+  },
+  {
+    "revision": "f72ebea6ee966f17be53",
+    "url": "/skipper113/testhub/static/js/2.8a0b4a8b.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/skipper113/testhub/static/js/2.8a0b4a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74be43bfdd09cc81f23",
+    "url": "/skipper113/testhub/static/js/main.816084df.chunk.js"
+  },
+  {
+    "revision": "551eebb9234d0da245f1",
+    "url": "/skipper113/testhub/static/js/runtime-main.990050c7.js"
+  }
+]);
